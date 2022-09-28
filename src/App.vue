6605fde715c5b86b59f9cdb566f1a2c1c15fea0d
Erhan Yaşar <mail@erhanyasar.com.tr>
2022-09-28 12:19:32 +0300

    fixes indentation

--- src/App.vue
+++ src/App.vue
@@ -6,45 +6,43 @@
 </template>
 
 <script>
-import axios from 'axios';
-import FormInput from './components/FormInput.vue'
-import ResultList from './components/ResultList.vue'
+  import axios from 'axios';
+  import FormInput from './components/FormInput.vue'
+  import ResultList from './components/ResultList.vue'
 
-export default {
-  name: 'MainApp',
-  components: {
-    FormInput,
-    ResultList
-  },
-  data: function() {
-    return {
-      allProducts: []
-    }
-  },
-  mounted() {
-    axios
-      .get("./products.json")
-      .then(response => {
-        //console.log(response.data.products);
-        this.allProducts = response.data.products
-      });
-  },
-  methods: {
-    filterProducts: function(searchParam, event) {
-      alert(event.target.payload[0]);
-      console.log(searchParam);
+  export default {
+    name: 'MainApp',
+    components: {
+      FormInput,
+      ResultList
+    },
+    data: function() {
+      return {
+        allProducts: []
+      }
+    },
+    mounted() {
+      axios
+        .get("./products.json")
+        .then(response => {
+          this.allProducts = response.data.products
+        });
+    },
+    methods: {
+      filterProducts: function(searchParam, event) {
+        alert(event.target.payload[0]);
+      }
     }
   }
-}
 </script>
 
 <style>
-#main-app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #main-app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
