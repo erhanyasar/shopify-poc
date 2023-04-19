f2b05fdd09600225ba9846334905638a3c3b4d6b
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-19 18:57:26 +0300

    deletes unnecessary style tags and fixes variable error

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -20,13 +20,10 @@ export default {
   name: 'FormInput',
   data: function() {
     return {
+      searchParam: '',
       title: 'Filter Products for ',
       link: 'Toolio Shopify Account'
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
